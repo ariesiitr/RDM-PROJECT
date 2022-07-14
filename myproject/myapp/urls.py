639511981121168b5093d2c42a5f0179d62dd3dc
Suvanshnarang <narangsuvansh01@gmail.com>
2022-07-14 17:31:20 +0530

    addedq1

--- myproject/myapp/urls.py
+++ myproject/myapp/urls.py
@@ -2,10 +2,7 @@ from turtle import home
 from django.urls import path,include
 from . import views
 from django.contrib import admin 
-from django.conf import settings
-from django.conf.urls.static import static
-from django.views.static import serve
-from django.conf.urls import url
+
 urlpatterns =[
     path('register/',views.register,name="register"),
     path('login/',views.login,name="login"),
@@ -21,6 +18,5 @@ urlpatterns =[
     path('cart3/',views.cart3,name="cart3"),
     path('orders/',views.orders,name="orders"),
     path('about/',views.about,name="about"),
-    url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
-    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
+
 ]

8ca770e64d9389aa2f2f1612ffc1449814a9f9f2
Suvanshnarang <narangsuvansh01@gmail.com>
2022-07-14 17:27:47 +0530

    added3

--- myproject/myapp/urls.py
+++ myproject/myapp/urls.py
@@ -1,7 +1,9 @@
 from turtle import home
 from django.urls import path,include
 from . import views
+from django.contrib import admin 
 from django.conf import settings
+from django.conf.urls.static import static
 from django.views.static import serve
 from django.conf.urls import url
 urlpatterns =[
--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -120,7 +120,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
-MEDIA_URL = '/media/'
+MEDIA_URL = 'media/'
 STATICFILES_DIRS=(os.path.join(BASE_DIR,"static/"),)
 
 # Default primary key field type
